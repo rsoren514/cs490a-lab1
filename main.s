2179edc7bbb7d4e62aa6776bd49fbc9c78ae7b9e
Alan Peters <alan3051@gmail.com>
2016-03-13 12:44:45 -0600

    Updating with working code

--- main.s
+++ main.s
@@ -30,29 +30,49 @@ SYSCTL_RCGCGPIO_R       EQU   0x400FE608
       THUMB
       EXPORT  Start
 Start
-		; Turn on clock for Port E
-		LDR			R3, =0x10
-		LDR			R4, =SYSCTL_RCGCGPIO_R
-		LDR			R5, [R4]
-		ORR			R5, R5, R3
-		STR			R5, [R4]
-		
-		; Wait 4 cycles
-		NOP
-		NOP
-		; Set direction flags for input mask
-		LDR			R3, =0x38
-		LDR			R4, =GPIO_PORTE_DIR_R
-		STR			R3, [R4]
-		; Set DEN flags for input mask
-		LDR			R4, =GPIO_PORTE_DEN_R
-		STR			R3, [R4]
+      ;Enable porte clock
+      LDR         R3, =0x10
+      LDR         R4, =SYSCTL_RCGCGPIO_R
+      LDR         R5, [R4]
+      ORR         R5, R5, R3
+      STRB        R5, [R4]  ; need to wait 4 clocks before modifying other PORTE
+      
+      LDR         R8, =0x04 ;LED Bit      ;1 clk
+      
+      ;invert LED Bit AKA NOT LED Bit
+      LDR         R9, =0xFFFFFFFF         ;2 clk
+      EOR         R9, R9, R8;             ;3 clk
+      
+      ; set port direction out for LED pin
+      LDR         R4, =GPIO_PORTE_DIR_R   ;4 clk
+      STRB        R8, [R4]
+      
+      ;enable needed digital IO pins
+      LDR         R3, =0x3C ; 0b 0011 1100
+      LDR         R4, =GPIO_PORTE_DEN_R
+      STRB        R3, [R4]
+      
+      ;for comparison in loop
+      LDR         R10, =0x38
+      LDR         R6, =0x00 
+      
+      ;clear AMSEL & AFSEL & PCTL
+      LDR         R4, =GPIO_PORTE_AMSEL_R
+      STRB        R6, [R4]
+      LDR         R4, =GPIO_PORTE_AFSEL_R
+      STRB        R6, [R4]
+      LDR         R4, =GPIO_PORTE_PCTL_R
+      STRB        R6, [R4]
 loop
-		; Load input pins from Port E
-		LDR			R4, =GPIO_PORTE_DATA_R
-		LDR			R5, [R4]
-		AND			R6, R5, R3
-		B			loop
+      LDR         R4, =GPIO_PORTE_DATA_R
+      LDR         R5, [R4]
+      AND         R7, R5, R10
+      TEQ         R7, R6
+      ORREQ       R5, R8 ;if equal then turn on LED
+      ANDNE       R5, R9 ;if not equal then turn off LED
+      STRB        R5, [R4]
+      
+      B   loop
 
 
       ALIGN        ; make sure the end of this section is aligned
