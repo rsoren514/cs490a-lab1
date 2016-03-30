7d3cc08144a35b99e01cd0683250c6d73fcef821
Randy Sorensen <gameforge1@gmail.com>
2016-03-29 19:43:33 -0600

    Added labs 3 and 4 - 4 not yet complete.

---
+++ lab4/main.s
@@ -0,0 +1,114 @@
+;****************** main.s ***************
+; Program written by: ***Your Names**update this***
+; Date Created: 1/22/2016 
+; Last Modified: 1/22/2016 
+; Section ***Tuesday 1-2***update this***
+; Instructor: ***Ramesh Yerraballi**update this***
+; Lab number: 4
+; Brief description of the program
+;   If the switch is presses, the LED toggles at 8 Hz
+; Hardware connections
+;  PE1 is switch input  (1 means pressed, 0 means not pressed)
+;  PE0 is LED output (1 activates external LED on protoboard) 
+;Overall functionality of this system is the similar to Lab 3, with three changes:
+;1-  initialize SysTick with RELOAD 0x00FFFFFF 
+;2-  add a heartbeat to PF2 that toggles every time through loop 
+;3-  add debugging dump of input, output, and time
+; Operation
+;	1) Make PE0 an output and make PE1 an input. 
+;	2) The system starts with the LED on (make PE0 =1). 
+;   3) Wait about 62 ms
+;   4) If the switch is pressed (PE1 is 1), then toggle the LED once, else turn the LED on. 
+;   5) Steps 3 and 4 are repeated over and over
+
+
+SWITCH                  EQU 0x40024008  ;PE1
+LED                     EQU 0x40024004  ;PE0
+SYSCTL_RCGCGPIO_R       EQU 0x400FE608
+SYSCTL_RCGC2_GPIOE      EQU 0x00000010   ; port E Clock Gating Control
+SYSCTL_RCGC2_GPIOF      EQU 0x00000020   ; port F Clock Gating Control
+GPIO_PORTE_DATA_R       EQU 0x400243FC
+GPIO_PORTE_DIR_R        EQU 0x40024400
+GPIO_PORTE_AFSEL_R      EQU 0x40024420
+GPIO_PORTE_PUR_R        EQU 0x40024510
+GPIO_PORTE_DEN_R        EQU 0x4002451C
+GPIO_PORTF_DATA_R       EQU 0x400253FC
+GPIO_PORTF_DIR_R        EQU 0x40025400
+GPIO_PORTF_AFSEL_R      EQU 0x40025420
+GPIO_PORTF_DEN_R        EQU 0x4002551C
+NVIC_ST_CTRL_R          EQU 0xE000E010
+NVIC_ST_RELOAD_R        EQU 0xE000E014
+NVIC_ST_CURRENT_R       EQU 0xE000E018
+           THUMB
+           AREA    DATA, ALIGN=4
+SIZE       EQU    50
+;You MUST use these two buffers and two variables
+;You MUST not change their names
+;These names MUST be exported
+           EXPORT DataBuffer  
+           EXPORT TimeBuffer  
+           EXPORT DataPt [DATA,SIZE=4] 
+           EXPORT TimePt [DATA,SIZE=4]
+DataBuffer SPACE  SIZE*4
+TimeBuffer SPACE  SIZE*4
+DataPt     SPACE  4
+TimePt     SPACE  4
+
+    
+      ALIGN          
+      AREA    |.text|, CODE, READONLY, ALIGN=2
+      THUMB
+      EXPORT  Start
+      IMPORT  TExaS_Init
+
+
+Start BL   TExaS_Init  ; running at 80 MHz, scope voltmeter on PD3
+      
+      BL      Init                            ; initialize
+
+; initialize Port E
+; initialize Port F
+; initialize debugging dump, including SysTick
+
+
+      CPSIE   I    ; TExaS voltmeter, scope runs on interrupts
+      
+      BL      Main                            ; run program (forever)
+      
+
+loop  BL   Debug_Capture
+;heartbeat
+; Delay
+;input PE1 test output PE0
+	  B    loop
+
+
+
+
+
+;------------Debug_Init------------
+; Initializes the debugging instrument
+; Input: none
+; Output: none
+; Modifies: none
+; Note: push/pop an even number of registers so C compiler is happy
+Debug_Init
+      
+; init SysTick
+
+      BX LR
+
+;------------Debug_Capture------------
+; Dump Port E and time into buffers
+; Input: none
+; Output: none
+; Modifies: none
+; Note: push/pop an even number of registers so C compiler is happy
+Debug_Capture
+
+      BX LR
+
+
+    ALIGN                           ; make sure the end of this section is aligned
+    END                             ; end of file
+        
